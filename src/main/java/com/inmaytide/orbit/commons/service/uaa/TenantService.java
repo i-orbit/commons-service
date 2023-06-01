b4fc838eef20c3a1ca5c4061f9f0106370fcee84
inmaytide <inmaytide@gmail.com>
2023-06-01 16:10:12 +0800

    查询租户列表接口

---
+++ src/main/java/com/inmaytide/orbit/commons/service/uaa/TenantService.java
@@ -0,0 +1,15 @@
+package com.inmaytide.orbit.commons.service.uaa;
+
+import com.inmaytide.orbit.commons.domain.Tenant;
+
+import java.util.List;
+
+/**
+ * @author inmaytide
+ * @since 2023/6/1
+ */
+public interface TenantService {
+
+    List<Tenant> all();
+
+}
